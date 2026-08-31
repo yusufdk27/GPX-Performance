0cbef322a0ca04b5b7aef8c7b2675ef535787187
Yusuf Dwi Kurniawan <93754445+yusufdk27@users.noreply.github.com>
2026-08-31 20:53:17 +0700

    Initial Commit

---
+++ GPX-Performance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPX-Performance
+//
+//  Created by Yusuf Dwi Kurniawan on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPX-Performance/GPX_PerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPX_PerformanceApp.swift
+//  GPX-Performance
+//
+//  Created by Yusuf Dwi Kurniawan on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct GPX_PerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
